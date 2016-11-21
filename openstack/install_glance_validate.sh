6acd1dd6093a3edd606853355f5f8e3a6b66c37f
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2016-11-21 15:40:34 +0800

    环境的验证

---
+++ openstack/install_glance_validate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# CentOS 7 
+# OpenStack = mitaka
+# node = controller
+#
+#
+
+#----------------------------[ 验证镜像服务 ]----------------------------------------
+# 获得 admin 凭证来获取只有管理员能执行的命令的访问权限：
+. /root/admin-openrc
+#下载源镜像：
+cd /tmp
+wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
+# 将下载的镜像上传到openstack环境
+openstack image create "cirros" \
+  --file cirros-0.3.4-x86_64-disk.img \
+  --disk-format qcow2 --container-format bare \
+  --public
+# 确认镜像的上传并验证属性：
+openstack image list
---
+++ openstack/install_keystone_validate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# CentOS 7 
+# OpenStack = mitaka
+# node = controller
+#
+#
+
+#----------------------------[ 验证keystone ]----------------------------------------
+vim /etc/keystone/keystone-paste.ini
+# 撤销临时环境变量``OS_AUTH_URL``和``OS_PASSWORD``
+unset OS_AUTH_URL OS_PASSWORD
+# 作为 admin 用户，请求认证令牌：
+expect<<END
+openstack --os-auth-url http://${CONTROLLER_HOSTNAME}:35357/v3 \
+  --os-project-domain-name default --os-user-domain-name default \
+  --os-project-name admin --os-username admin token issue
+expect {
+"Password:" {send "${ADMIN_PASS}\n"}
+}
+interact
+END
+# 作为``demo`` 用户，请求认证令牌：
+expect<<END
+spawn openstack --os-auth-url http://${CONTROLLER_HOSTNAME}:5000/v3 \
+  --os-project-domain-name default --os-user-domain-name default \
+  --os-project-name demo --os-username demo token issue
+expect {
+"Password:" {send "${DEMO_PASS}\n"}
+}
+interact
+END
