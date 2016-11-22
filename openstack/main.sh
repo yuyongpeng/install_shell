50e08a9606d6ffb22b4fc9076b42f8e0d387de9a
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2016-11-22 17:07:38 +0800

    update

--- openstack/main.sh
+++ openstack/main.sh
@@ -8,8 +8,8 @@
 
 # 包含 配置信息
 cd $(dirname $0)
-. openstack_config.sh
-. install_controller_base.sh
+#. openstack_config.sh
+#. install_controller_base.sh
 . install_keystone.sh
 . install_glance.sh
 . install_compute_controller.sh
