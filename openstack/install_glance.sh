9fe45fcc2ec61747dd3ad0b0c4fa312bd83e3776
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2016-11-22 12:27:29 +0800

    update33

--- openstack/install_glance.sh
+++ openstack/install_glance.sh
@@ -12,7 +12,9 @@ cd $(dirname $0)
 mysql -uroot -p${MYSQL_ROOT_PASS} -e "create database glance default charset=utf8;"
 mysql -uroot -p${MYSQL_ROOT_PASS} -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '${GLANCE_DBPASS}';"
 mysql -uroot -p${MYSQL_ROOT_PASS} -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY '${GLACNE_DBPASS}';"
+mysql -uroot -p${MYSQL_ROOT_PASS} -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'${CONTROLLER_HOSTNAM}' IDENTIFIED BY '${GLACNE_DBPASS}';"
 mysql -uroot -p${MYSQL_ROOT_PASS} -e "flush privileges;"
+
 
 # 获得访问权限
 . ${OPENRC_PATH}/${OPENRC_ADMIN_USER}
