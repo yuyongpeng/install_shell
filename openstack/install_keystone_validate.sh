16f45339c367e8d26455c6c07c244f4dd1c60bb3
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2016-11-22 14:49:47 +0800

    修改了keystone-parse文件

--- openstack/install_keystone_validate.sh
+++ openstack/install_keystone_validate.sh
@@ -8,7 +8,13 @@
 cd $(dirname $0)
 . ./openstack_config.sh
 #----------------------------[ 验证keystone ]----------------------------------------
-vim /etc/keystone/keystone-paste.ini
+keystone-paste=/etc/keystone/keystone-paste.in
+#vim /etc/keystone/keystone-paste.ini
+cp /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini_bak
+openstack-config --set ${keystone-paste} pipeline:public_api pipeline "cors sizelimit http_proxy_to_wsgi osprofiler url_normalize request_id admin_token_auth build_auth_context token_auth json_body ec2_extension public_service"
+openstack-config --set ${keystone-paste} pipeline:admin_api pipeline "cors sizelimit http_proxy_to_wsgi osprofiler url_normalize request_id admin_token_auth build_auth_context token_auth json_body ec2_extension s3_extension admin_service"
+openstack-config --set ${keystone-paste} pipeline:api_v3 pipeline "cors sizelimit http_proxy_to_wsgi osprofiler url_normalize request_id admin_token_auth build_auth_context token_auth json_body ec2_extension_v3 s3_extension service_v3"
+
 # 撤销临时环境变量``OS_AUTH_URL``和``OS_PASSWORD``
 unset OS_AUTH_URL OS_PASSWORD
 # 作为 admin 用户，请求认证令牌：
