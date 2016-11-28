386ed0cdc19a5694c36074fc07f5f3635b212a2f
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2016-11-28 17:23:45 +0800

    update

--- mongodb/install_mongo.sh
+++ mongodb/install_mongo.sh
@@ -16,6 +16,18 @@
 #    esac  
 #done 
 mongoRepoFile=/etc/yum.repos.d/mongodb-org-3.2.repo
+mongoDbPath=/data/mongodb
+# root 角色的用户
+rootAsUser=root
+rootAsPass=modernmedia
+# userAdminAnyDatabase 角色用户
+userAdminAnyDatabase_user1=user1
+userAdminAnyDatabase_pass1=pass1
+# readWrite 角色用户
+readWrite_user1=user1
+readWrite_pass1=pass1
+
+
 touch $mongoRepoFile
 cat > $mongoRepoFile <<END
 [mongodb-org-3.2]
@@ -27,3 +39,11 @@ gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
 END
 
 yum install -y mongodb-org
+mkdir -p ${mongoDbPath}
+chown -R mongod.mongod ${mongoDbPath}
+cp mongod.conf /etc/
+sed -i 's/dataMongoPath/${mongoDbPath/\//\\\/}/g' /etc/mongod.conf
+
+# 初始化数据库的权限用户
+
+
