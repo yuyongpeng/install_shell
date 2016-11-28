d43d1f7be512b010b739187f0be751aeba9b8a53
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2016-11-28 12:45:42 +0800

    mongo init

---
+++ mongodb/install_mongo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#while getopts "v:bc" arg  
+#do  
+#    case $arg in  
+#        a)  
+#            #参数存在$OPTARG中  
+#            echo "a's arg:$OPTARG" ;;  
+#        v)  
+#            echo "b" ;;  
+#        c)  
+#            echo "c" ;;   
+#        ?)  
+#            #当有不认识的选项的时候arg为?  
+#            echo "unkonw argument" exit 1 ;;  
+#    esac  
+#done 
+mongoRepoFile=/etc/yum.repos.d/mongodb-org-3.2.repo
+touch $mongoRepoFile
+cat > $mongoRepoFile <<END
+[mongodb-org-3.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
+END
+
+#yum install mongodb
